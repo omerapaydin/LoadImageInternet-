1ea3e770c2ba004befeb5544277a6c90681cfaec
Ömer Apaydın <omerapaydin90@gmail.com>
2025-01-04 21:19:51 +0300

    Finish

--- LoadImageInternet?/ContentView.swift
+++ LoadImageInternet?/ContentView.swift
@@ -7,6 +7,24 @@
 
 import SwiftUI
 
+extension Image {
+    func imageModifier() -> some View {
+        self
+            .resizable()
+            .scaledToFit()
+    }
+    
+    func iconModifier() -> some View {
+        self
+            .imageModifier()
+            .frame(maxWidth:128)
+            .foregroundColor(.purple)
+            .opacity(0.5)
+    }
+    
+}
+
+
 struct ContentView: View {
     
     private let imageURL : String = "https://credo.academy/credo-academy@3x.png"
@@ -23,8 +41,53 @@ struct ContentView: View {
         
         // MARK: - 3. Placeholder
         
+        /*
+        AsyncImage(url: URL(string: imageURL)) { image in
+            image
+                .imageModifier()
+        } placeholder: {
+            Image(systemName: "photo.circle.fill")
+                .iconModifier()
+                
+        }
+        .padding(40)
+        */
         
+        // MARK: - 4. Phase
         
+        /*
+        AsyncImage(url: URL(string: imageURL)) { phase in
+            
+            if let image = phase.image {
+                image.imageModifier()
+            }else if phase.error != nil {
+                Image(systemName: "ant.circle.fill").iconModifier()
+            }else {
+                Image(systemName: "photo.circle.fill").iconModifier()
+            }
+            
+        }.padding(40)
+        */
+        
+        // MARK: - 5. Animation
+        
+        AsyncImage(url: URL(string: imageURL),transaction: Transaction(animation: .spring(response:0.5,dampingFraction: 0.6,blendDuration: 0.25))) { phase in
+            switch phase {
+            case .success(let image):
+                image.imageModifier()
+                    //.transition(.move(edge: .bottom))
+                    //.transition(.slide)
+                    .transition(.scale)
+                
+            case .failure(_):
+                Image(systemName: "ant.circle.fill").iconModifier()
+            case .empty:
+                Image(systemName: "photo.circle.fill").iconModifier()
+            @unknown default:
+                ProgressView()
+            }
+
+        }
         
         
         
