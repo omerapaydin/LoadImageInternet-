967620e0ef8a1ece2b60df72ef075a4016554d8c
Ömer Apaydın <omerapaydin90@gmail.com>
2025-01-04 20:36:39 +0300

    First

--- LoadImageInternet?/ContentView.swift
+++ LoadImageInternet?/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private let imageURL : String = "https://credo.academy/credo-academy@3x.png"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        // MARK: - 1. Basic
+        
+       // AsyncImage(url:URL(string: imageURL))
+            
+        // MARK: - 2. Scale
+        
+        // AsyncImage(url:URL(string: imageURL),scale: 3.0)
+        
+        // MARK: - 3. Placeholder
+        
+        
+        
+        
+        
+        
     }
 }
 
