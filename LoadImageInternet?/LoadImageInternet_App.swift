8f8f91d62db3688cd1e41a6a8a1fb00af86c981e
Ömer Apaydın <omerapaydin90@gmail.com>
2025-01-04 20:26:24 +0300

    Initial Commit

---
+++ LoadImageInternet?/LoadImageInternet_App.swift
@@ -0,0 +1,17 @@
+//
+//  LoadImageInternet_App.swift
+//  LoadImageInternet?
+//
+//  Created by Ömer on 4.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct LoadImageInternet_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LoadImageInternet?Tests/LoadImageInternet_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  LoadImageInternet_Tests.swift
+//  LoadImageInternet?Tests
+//
+//  Created by Ömer on 4.01.2025.
+//
+
+import Testing
+@testable import LoadImageInternet_
+
+struct LoadImageInternet_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
